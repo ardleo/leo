8ad4456510b173d84b0337c8698351c5ba803b62
ardall.leo <ardall.leo@erraipasifik.com>
2014-10-03 11:15:25 +0700

    #time 2h #comment Added sync method to move certain key in cookie into local storage.

--- leo.js
+++ leo.js
@@ -1,10 +1,31 @@
 /**
+ * Azure Remote VM Switcher
+ * server.js
+ *
+ * Copyright 2014 Ring2 Communications LTD. All rights reserved.
+ */
+
+
+/**
 * @namespace leo
 */
 leo = {
+	init : function(){
+		if ( !String.prototype.contains ) {
+			String.prototype.contains = function() {
+				return String.prototype.indexOf.apply( this, arguments ) !== -1;
+			};
+		}
+	},
+	/**
+	* @namespace config
+	* @property {numeric} EXPIRATION_DATE - Expiration date for cookie only, in day unit.
+	* @property {boolean} FORCE_USE_COOKIE - By default, this library will try to use local storage and fallback to cookie if the browser doesn't support. Set it to true, to force using cookie.
+    */
 	config: {
-		EXPIRATION_DATE : 30, // in day unit
-		FORCE_USE_COOKIE : true
+		EXPIRATION_DATE : 30,
+		FORCE_USE_COOKIE : true,
+		COOKIE_KEY_TO_MOVE_TO_LS : ['number']
 	},
 	util : {
 		cookie : {},
@@ -12,6 +33,8 @@ leo = {
 		storage : {}
 	}	
 }
+
+leo.init();
 
 /* general storage helper */
 leo.util.storage = {
@@ -48,6 +71,26 @@ leo.util.storage = {
 			leo.util.ls.removeAll();
 		} else {
 			leo.util.cookie.removeAll();
+		}
+	},
+	// this method is for moving whatever keys in cookie that we want to move to local storage. 
+	// any keys that we want to move have to be declared in COOKIE_KEY_TO_MOVE_TO_LS.
+	sync : function(){		
+		var cookies = leo.util.cookie.readAll();
+		for ( cookie in cookies ){
+			console.log( cookie );
+			var theCookie = $.grep( leo.config.COOKIE_KEY_TO_MOVE_TO_LS, function( str ){
+				return cookie.contains( str );
+			})
+			var theCookieLength = theCookie.length;
+			if ( theCookieLength > 0 ){		
+				var i;
+				for( i=0;i < theCookieLength; i++ ){
+					console.log( cookie );
+					leo.util.ls.write( cookie, cookies[cookie] );
+					leo.util.cookie.remove( cookie, cookies[cookie] );
+				}
+			}
 		}
 	}
 }
